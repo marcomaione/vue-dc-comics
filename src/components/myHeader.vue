da1dbcb521978622bd0948683d159e37b5e8ae55
marco maione <maionemarco14@gmail.com>
2022-02-21 15:52:40 +0100

    aggiornamento

---
+++ src/components/myHeader.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+       <img src="../assets/img/dc-logo.png" alt="">
+       
+   </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'myHeader',
+  
+}
+</script>

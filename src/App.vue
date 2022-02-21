af2e813838b4c1e7796b775f2dddffc06f4f625a
marco maione <maionemarco14@gmail.com>
2022-02-21 17:06:50 +0100

    aggiornamento

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <myHeader></myHeader>
     <myMain></myMain>
+    <myFooter></myFooter>
   </div>
 </template>
 
 <script>
 import myHeader from './components/myHeader.vue'
 import myMain from './components/myMain.vue'
+import myFooter from './components/myFooter.vue'
 
 export default {
   name: 'App',
   components: {
     myHeader,
-    myMain
+    myMain,
+    myFooter
   }
 }
 </script>
---
+++ src/components/myFooter.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="up">
+            <div class="items">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="items">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="items">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="items">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="items">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+        </div>
+        <div class="midle">
+            ciao
+        </div>
+        <div class="under"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'myFooter',
+  
+}
+</script>
+
+<style lang="scss">
+
+.container {
+    width: 100%;
+    .up {
+        display: flex;
+        justify-content: center;
+        min-height: 250px;
+        background-color:#0282f9 ;
+    }
+    .items {
+        display: flex;
+        align-items: center;
+    }
+    .midle {
+        min-height: 300px;
+        background-image: url("../assets/img/footer-bg.jpg");
+    }
+}
+
+</style>

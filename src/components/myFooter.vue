6efa4b0736f9f1f382aa0b33c8f05a28ed1e3201
marco maione <maionemarco14@gmail.com>
2022-02-21 17:41:34 +0100

    aggiornamento

--- src/components/myFooter.vue
+++ src/components/myFooter.vue
@@ -6,24 +6,28 @@
                 <span>DIGITAL COMICS</span>
             </div>
             <div class="items">
-                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
                 <span>DIGITAL COMICS</span>
             </div>
             <div class="items">
-                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
                 <span>DIGITAL COMICS</span>
             </div>
             <div class="items">
-                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
                 <span>DIGITAL COMICS</span>
             </div>
             <div class="items">
-                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
                 <span>DIGITAL COMICS</span>
             </div>
         </div>
         <div class="midle">
-            ciao
+            <div class="midle-left"></div>
+            <div class="midle-right">
+                <img src="../assets/img/dc-logo-bg.png" alt="">
+            </div>
+            
         </div>
         <div class="under"></div>
     </div>
@@ -40,11 +44,22 @@ export default {
 
 .container {
     width: 100%;
+    background-color:#0282f9 ;
     .up {
+        width: 80%;
+        width: 100%;
+        margin: 0px auto;
         display: flex;
         justify-content: center;
+        align-items: center;
         min-height: 250px;
         background-color:#0282f9 ;
+    }
+    img {
+        max-width: 60px;
+    }
+    span {
+        margin:15px;
     }
     .items {
         display: flex;
@@ -53,6 +68,24 @@ export default {
     .midle {
         min-height: 300px;
         background-image: url("../assets/img/footer-bg.jpg");
+        display: flex;
+        justify-content: center;
+        width: auto;
+        height: 200px;
+    }
+    .midle-left {
+        border:solid 1px black;
+        width: 40%;
+    }
+    .midle-right {
+        border:solid 1px black;
+        width: 40%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .midle-right img {
+        max-width:100%;
     }
 }
 

2cfec069a858b40284424d046b2e3e9e8c67ef29
marco maione <maionemarco14@gmail.com>
2022-02-21 15:56:33 +0100

    aggiornamento

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -1,6 +1,38 @@
 <template>
    <div>
        <img src="../assets/img/dc-logo.png" alt="">
+       <ul>
+           <li>
+               <a href="#">CHARACTERS</a>
+           </li>
+            <li>
+               <a href="#">COMICS</a>
+           </li>
+            <li>
+               <a href="#">MOVIES</a>
+           </li>
+            <li>
+               <a href="#">TV</a>
+           </li>
+            <li>
+               <a href="#">GAMES</a>
+           </li>
+            <li>
+               <a href="#">COLLECTIBLES</a>
+           </li>
+            <li>
+               <a href="#">VIDEOS</a>
+           </li>
+            <li>
+               <a href="#">FANS</a>
+           </li>
+            <li>
+               <a href="#">NEWS</a>
+           </li>
+            <li>
+               <a href="#">SHOP</a>
+           </li>
+       </ul>
        
    </div>
     
